925a35e92ff92b6ad61b0bdf1507aa34d6420ad2
chamber-prime <aravindsubramanian@presidio.com>
2026-08-17 14:04:26 +0530

    Added identity data blocks

--- main.tf
+++ main.tf
@@ -16,6 +16,15 @@ provider "azurerm" {
   features {}
 }
 
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_role_assignment" "me_blob" {
+  scope                = azurerm_storage_account.state.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
 variable "location" {
   type    = string
   default = "eastus2"
