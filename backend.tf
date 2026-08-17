9d1bcae0d4bdd49c14815ecce0c5e3bfd1410334
chamber-prime <aravindsubramanian@presidio.com>
2026-08-17 14:04:58 +0530

    Modified storage account

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate-lab"
-    storage_account_name = "sttfstatelabix8jf4"
+    storage_account_name = "sttfstatelabhpx5ry"
     container_name       = "tfstate"
     key                  = "lab.tfstate"
     use_azuread_auth     = true
